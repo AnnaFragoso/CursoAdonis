9d778fdd94b46cd8489a198939c9a1dcd4de3568
AnnaFragoso <anna.barbara.fragoso@hotmail.com>
2019-12-19 13:02:45 -0300

    Validação das outras rotas

--- app/Exceptions/Handler.js
+++ app/Exceptions/Handler.js
@@ -2,33 +2,16 @@
 
 const BaseExceptionHandler = use('BaseExceptionHandler');
 const Env = use('Env');
-const Youch = use('Youch');
+const Youch = require('youch');
 
-/**
- * This class handles all exceptions thrown during
- * the HTTP request lifecycle.
- *
- * @class ExceptionHandler
- */
 class ExceptionHandler extends BaseExceptionHandler {
-  /**
-   * Handle exception thrown during the HTTP lifecycle
-   *
-   * @method handle
-   *
-   * @param  {Object} error
-   * @param  {Object} options.request
-   * @param  {Object} options.response
-   *
-   * @return {void}
-   */
   async handle (error, { request, response }) {
     if(error.name === 'ValidationException') {
       response.status(error.status).send(error.message);
     }
 
     if(Env.get('NODE_ENV') === 'development') {
-      const youch = new youch(error, request.request);
+      const youch = new Youch(error, request.request);
       const errorJSON = await youch.toJSON();
       response.status(error.status).send(errorJSON);
     }
@@ -36,16 +19,6 @@ class ExceptionHandler extends BaseExceptionHandler {
     return response.status(error.status);
   }
 
-  /**
-   * Report exception for logging or debugging.
-   *
-   * @method report
-   *
-   * @param  {Object} error
-   * @param  {Object} options.request
-   *
-   * @return {void}
-   */
   async report (error, { request }) {
     console.log(error);
   }
